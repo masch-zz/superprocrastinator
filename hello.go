c29430f028f458d752e7bf1ad741923cf804a4a1
masch <the.masch@gmail.com>
2017-08-21 20:37:08 -0300

    added vendor folder

--- hello.go
+++ hello.go
@@ -4,12 +4,30 @@ import (
 	"fmt"
 	"net/http"
 
-	"appengine"
-	"appengine/user"
+	"golang.org/x/net/context"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/user"
+	"time"
 )
+
+// [START greeting_struct]
+type Greeting struct {
+	Author  string
+	Content string
+	Date    time.Time
+}
+
+// [END greeting_struct]
 
 func init() {
 	http.HandleFunc("/", handler)
+}
+
+// guestbookKey returns the key used for all guestbook entries.
+func guestbookKey(c context.Context) *datastore.Key {
+	// The string "default_guestbook" here could be varied to have multiple guestbooks.
+	return datastore.NewKey(c, "Guestbook", "default_guestbook", 0, nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -31,6 +49,23 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	// [END if_user]
+
+	g := Greeting{
+		Content: "Test",
+		Date:    time.Now(),
+	}
+
+	// We set the same parent key on every Greeting entity to ensure each Greeting
+	// is in the same entity group. Queries across the single entity group
+	// will be consistent. However, the write rate to a single entity group
+	// should be limited to ~1/second.
+	key := datastore.NewIncompleteKey(c, "Greeting", guestbookKey(c))
+	_, err := datastore.Put(c, key, &g)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	// [START output]
 	fmt.Fprintf(w, "Hello, %v!", u)
 	// [END output]
